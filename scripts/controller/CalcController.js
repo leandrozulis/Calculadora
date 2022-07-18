8645b138dd78277e35416e96dcffaa008a42ca45
leandrozulis <leandro_zulli@hotmail.com>
2022-07-18 15:53:25 -0300

    Manipulação da DOM e getters e setters

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,39 @@
+class CalcController{
+
+  constructor() {
+
+    this._displayCalc = "0";
+    this._currentDate;
+    this.initialize();
+
+  }
+
+  initialize() {
+
+    let displayCalcEl = document.querySelector("#display");
+    let dateEl = document.querySelector("#data");
+    let timeEl = document.querySelector("#hora");
+
+    displayCalcEl.innerHTML = "4567";
+    dateEl.innerHTML = "18/07/2022";
+    timeEl.innerHTML = "15:51";
+
+  }
+
+  get displayCalc() {
+    return this._displayCalc;
+  }
+
+  set displayCalc(valor) {
+    this._displayCalc = valor;
+  }
+
+  get currentDate() {
+    return this._currentDate;
+  }
+
+  set currentDate(valor) {
+    this._currentDate = valor;
+  }
+
+}
