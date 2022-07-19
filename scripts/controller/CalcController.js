17c2b42d4b983b18b47482ed718ac166b8bb35ab
leandrozulis <leandro_zulli@hotmail.com>
2022-07-19 15:41:34 -0300

    copy ctrl+c e paste ctrl+v

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -16,6 +16,36 @@ class CalcController{
 
   }
 
+  pasteFromClipboard() {
+
+    document.addEventListener('paste', e => {
+
+      let text = e.clipboardData.getData('Text');
+
+      this.displayCalc = parseFloat(text);
+
+      console.log(text);
+
+    })
+
+  }
+
+  copyToClipboard() {
+
+    let input = document.createElement('input');
+
+    input.value = this.displayCalc;
+
+    document.body.appendChild(input);
+
+    input.select();
+
+    document.execCommand("Copy");
+
+    input.remove();
+
+  }
+
   initialize() {
 
     this.setDisplayDateTime();
@@ -27,14 +57,13 @@ class CalcController{
     },1000);
 
     this.setLastNumberToDisplay();
+    this.pasteFromClipboard();
 
   }
 
   initKeyBoard() {
 
     document.addEventListener('keyup', e => {
-
-      console.log(e.key);
 
       switch (e.key) {
       
@@ -71,6 +100,9 @@ class CalcController{
         case '8':
         case '9':
           this.addOperation(parseInt(e.key));
+          break;
+        case 'c':
+          if (e.ctrlKey) this.copyToClipboard()
           break;
     }
 
